8fc00eb5c7f40dcd079394da3272804996462f4c
James Allen <METACEO@users.noreply.github.com>
2020-04-12 19:37:28 -0500

    feat(demo): use Tailwind UI modal example component (#3)
    * feat(demo): use TailwindUI modal example component
    
    * cli(demo): yarn format:write

--- apps/ng-transition-demo/src/app/app.component.ts
+++ apps/ng-transition-demo/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-transition-demo';
-}
+export class AppComponent {}
