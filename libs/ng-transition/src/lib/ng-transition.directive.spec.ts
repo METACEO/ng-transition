2c9d5e7dfb2f4351473af9b44a8df43b3c44dedd
James Allen <james.allen@meta.ceo>
2020-04-12 20:11:13 -0500

    tests: satisfying basic test

--- libs/ng-transition/src/lib/ng-transition.directive.spec.ts
+++ libs/ng-transition/src/lib/ng-transition.directive.spec.ts
@@ -2,7 +2,7 @@ import { NgTransitionDirective } from './ng-transition.directive';
 
 describe('NgTransitionDirective', () => {
   it('should create an instance', () => {
-    const directive = new NgTransitionDirective();
+    const directive = new NgTransitionDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
