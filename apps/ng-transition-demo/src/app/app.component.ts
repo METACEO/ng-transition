53f18fa42a8ccd90c7911ec9064fa6f35b831156
James Allen <james.allen@meta.ceo>
2020-05-07 22:03:54 -0500

    feat: adding TUI sidebar example

--- apps/ng-transition-demo/src/app/app.component.ts
+++ apps/ng-transition-demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { TimeoutRef } from 'ng-refs';
 
 @Component({
@@ -7,12 +7,39 @@ import { TimeoutRef } from 'ng-refs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public opened = true;
+  opened = true;
+  openedLeftNav = false;
+  openedProfile = false;
+
+  @ViewChild('Profile')
+  public $profile: ElementRef<HTMLDivElement>;
 
   constructor(private readonly timeoutRef: TimeoutRef) {}
 
+  @HostListener('click', ['$event'])
+  public handleAutomaticUserPopoutClose($event) {
+    const $target = $event?.target;
+    const $profile = this.$profile?.nativeElement;
+    // If there's no good input or we're
+    // not even open, short-circuit.
+    if (!$target || !$profile || !this.openedProfile) {
+      return;
+    }
+    // Otherwise close the popout if the
+    // click is outside our container.
+    if (!$profile.contains($target)) {
+      this.openedProfile = false;
+    }
+  }
+
   public closeModal(): void {
     this.opened = false;
-    this.timeoutRef.nativeSet(() => (this.opened = true), 1000);
+    // this.timeoutRef.nativeSet(() => (this.opened = true), 1000);
+  }
+  public toggleLeftNav(): void {
+    this.openedLeftNav = !this.openedLeftNav;
+  }
+  public toggleProfile(): void {
+    this.openedProfile = !this.openedProfile;
   }
 }
