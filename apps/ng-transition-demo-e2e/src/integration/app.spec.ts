476185064a028a6011afd27580f35a10e046a38e
James Allen <METACEO@users.noreply.github.com>
2020-09-06 17:37:41 -0500

    feat: nx/ng 10 upgrade (#17)
    * cli: yarn run update --force
    
    * cli: yarn format:write
    
    * feat: use latest angular versions
    
    * test: basic e2e test suite
    
    * feat: build script
    
    * fix: jpg instead of png
    
    * cli: yarn format:write

--- apps/ng-transition-demo-e2e/src/integration/app.spec.ts
+++ apps/ng-transition-demo-e2e/src/integration/app.spec.ts
@@ -1,13 +1,11 @@
-import { getGreeting } from '../support/app.po';
+import { getModalMessage } from '../support/app.po';
 
 describe('ng-transition-demo', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to ng-transition-demo!');
+  it('should display a modal message', () => {
+    getModalMessage().contains(
+      'Are you sure you want to deactivate your account?'
+    );
   });
 });
