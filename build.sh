6e05e7538a8b067ec1d67cf0964bb5aebc107911
James Allen <james.allen@meta.ceo>
2020-09-06 17:07:41 -0500

    feat: build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo Download dependencies
+yarn
+
+echo Prove out quality
+yarn lint
+yarn affected:test
+yarn affected:e2e --headless
+
+echo Build our library
+yarn build ng-transition --prod
+
+echo Copy over README material
+cp ./README.md ./dist/libs/ng-transition
+cp ./README-ssr-browser-api-errors.png ./dist/libs/ng-transition
